5f8491778f84f98229f0e4aecf911a11b871d379
janikarh <janikarh@gmail.com>
2020-03-29 13:33:12 +0300

    Initial commit. Scaffold Python_Basic operational.

---
+++ README.md
@@ -0,0 +1,44 @@
+# Helpers
+
+Various CLI helper tools for the daily life on the Terminal.
+
+![project image](images/construction.svg)
+
+## Prerequisites
+
+This has been built for and tested on `macOS catalina 10.15.4` and `zsh`. This might work on other environments too, or not.
+
+## Helpers available
+
+1. [Scaffold new project](#scaffold-new-project)
+
+
+## Scaffold new Project
+
+Installation, so to speak.
+
+Add the following to your `.zshrc`:
+`alias scaffold=". [path-to-the-location]/jetzt2/scaffold.sh"`
+
+After sourcing with `source ~/.zshrc`, simply run `scaffold` and follow the prompt.
+
+### Project template alternatives
+
+At the moment, there are the following project options available.
+
+Common actions for all alternatives:
+
+1. Creates a new project directory, under current directory.
+
+#### Python_Basic
+
+Scaffold a basic Python project based on the system-level Python version (whatever `python3` returns).
+
+1. Creates *an virtual environment* named `venv` within the project directory, and activates it.
+2. Installs the latest versions of `pip` and `setuptools` as *production dependencies*, and `flake8` as *a development dependency* to the *venv*.
+3. Writes and **pins** the versions of the previous packages to `requirements.txt` and `requirements-dev.txt` respectively.
+
+
+#### Python_AWS_Serverless
+
+Does not do anything, at the moment :) Consider this a placeholder.
---
+++ scaffold.sh
@@ -0,0 +1,155 @@
+#!/usr/local/bin/zsh
+
+RED="`tput setaf 1`"
+GREEN="`tput setaf 2`"
+CYAN="`tput setaf 6`"
+BOLD="`tput bold`"
+NC="`tput sgr0`"
+
+function create_project_dir() {
+  echo "Creating new directory ..."
+
+  if mkdir "$1"; then
+    echo >$2 "Created dir"
+    return 0
+  else
+    echo >$2 "Cannot create dir"
+    return 1
+  fi
+}
+
+function create_python_venv() {
+  echo "${GREEN}Creating and activating new Python venv ...${NC}"
+  python3 -m venv venv
+  source 'venv/bin/activate'
+}
+
+function log_pkg_version() {
+  found_pkg_info=$(pip list | grep -iF "$1")
+  # echo "found_pkg_info: $found_pkg_info"
+  cleaned_pkg_info=$(echo "$found_pkg_info" | sed -e 's/[[:space:]]\{1,\}/|/g')
+  # echo "cleaned_pkg_info: $cleaned_pkg_info"
+  pkg_info_parts=("${(@s/|/)cleaned_pkg_info}")
+  pkg_info_name="${pkg_info_parts[1]}"
+  # echo "pkg_info_name: $pkg_info_name"
+  pkg_info_version="${pkg_info_parts[2]}"
+  # echo "pkg_info_version: $pkg_info_version"
+
+  echo "${GREEN}Logging $pkg_info_name==$pkg_info_version ...${NC}"
+  if [[ "$2" == "dev" ]]; then
+    echo "$pkg_info_name==$pkg_info_version" >>"requirements-dev.txt"
+  else
+    echo "$pkg_info_name==$pkg_info_version" >>"requirements.txt"
+  fi
+
+  unset found_pkg_info
+  unset cleaned_pkg_info
+  unset pkg_info_parts
+  unset pkg_info_name
+  unset pkg_info_version
+}
+
+function install_python_baseline() {
+  echo "${GREEN}Installing Python baseline ...${NC}"
+  python -m pip install --upgrade pip
+  log_pkg_version "pip" "prod"
+  python -m pip install --upgrade setuptools
+  log_pkg_version "setuptools" "prod"
+  python -m pip install flake8
+  log_pkg_version "flake8" "dev"
+
+  # python -m pip install python-dateutil
+  # log_pkg_version "python-dateutil" "prod"
+}
+
+function unset_variables() {
+  unset project_name
+  unset project_type
+}
+
+function end_on_error() {
+  echo "${RED}\nScaffolding ended with an error.${NC}"
+  unset_variables
+  return 1
+}
+
+function end_on_ok() {
+  echo "${GREEN}\nScaffolding ended ok. Happy hacking!${NC}"
+  unset_variables
+  return 0
+}
+
+function select_project_type() {
+  # List of possible project types
+  list=("Python_Basic" "Python_AWS_Serverless")
+
+  select var in $list; do
+    if [ x"$var" != x"" ]; then
+      echo $var
+    fi
+  return $var
+  done
+}
+
+function scaffold_python_basic() {
+  echo "${GREEN}Scaffolding Python_Basic with name $2 ...${NC}"
+  create_project_dir $2
+  retval=$?
+  if [[ "$retval" == 0 ]]; then
+    echo "${GREEN}Project Directory created succesfully.${NC}"
+    cd "$2"
+    # Make sure, that we really are in the new project dir,
+    # before installing anything.
+    if [[ "$(pwd)" == "$1/$2" ]]; then
+      echo "Successfully switched to project dir."
+      create_python_venv
+      install_python_baseline
+      return 0
+    else
+      echo "${RED}Something went wrong while creating or changing to the project dir $2 in $1${NC}"
+      return 1
+    fi
+  else
+    echo "${RED}Something went wrong while creating the project dir $2 in $1${NC}"
+    return 1
+  fi
+
+  return 1
+}
+
+echo "${GREEN}Start scaffolding.\n${NC}"
+
+# Ask for project name
+vared -p "Project name? " -c project_name
+project_name=$(echo "$project_name" | sed -e 's/[^A-Za-z0-9_-]/_/g')
+echo "project_name (with allowed naming scheme): $project_name"
+
+# Check, that the name is at least one character long.
+if [[ -z "$project_name" ]]; then
+  echo "${RED}ERROR: Project name must over 0 characters long.${NC}"
+  end_on_error
+else
+  # Check, if the project dir already exists. If it does, halt the execution.
+  if [[ -d "$project_name" ]]; then
+    echo "${RED}ERROR: Project directory $project_name already exists. Try with another name.${NC}"
+    end_on_error
+  else
+    echo "Dir $project_name does not exist. Moving to next step ..."
+
+    # Ask for project type
+    project_type=$(select_project_type)
+    echo "project_type: $project_type"
+
+    # Scaffold selected project type with entered name
+    if [[ "$project_type" == "Python_Basic" ]]; then
+      scaffold_python_basic $(pwd) $project_name
+      retvalue=$?
+      # echo "RETVAL2: $retvalue"
+      if [[ "$retvalue" == 0 ]]; then
+        end_on_ok
+      else
+        end_on_error
+      fi
+    fi
+  fi
+fi
